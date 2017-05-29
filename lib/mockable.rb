d740d55865627dece1338941e039ba55389d3e81
Dennis Walters <dwalters@engineyard.com>
2017-05-29 13:36:13 -0400

    Ported from Golang

--- lib/mockable.rb
+++ lib/mockable.rb
@@ -1,5 +1,18 @@
 require "mockable/version"
 
 module Mockable
-  # Your code goes here...
+  VARNAME = 'MOCKABLE'
+
+  def self.mocked?
+    ENV[VARNAME] ? true : false
+  end
+
+  def self.enable
+    ENV[VARNAME] = '1' unless mocked?
+  end
+
+  def self.disable
+    ENV.delete(VARNAME) if mocked?
+  end
+
 end
